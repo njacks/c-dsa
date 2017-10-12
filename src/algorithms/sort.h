80306dad597feebd564e0390fec6ee7ce41520a8
Nicholas Jackson <njacksdev@gmail.com>
2017-10-12 17:53:16 +0000

    add quick sort algorithm for linked lists

--- src/algorithms/sort.h
+++ src/algorithms/sort.h
@@ -6,5 +6,6 @@
 #define SORT_H
 
 int isort_list(void *data, int (*compare)(const void *key1, const void *key2));
+int qsort_list(void *data, int (*compare)(const void *key1, const void *key2));
 
 #endif
